7d743926889520073344068b63b2275bf2302492
philfromqueens <philpartyka@gmail.com>
2024-07-21 01:12:42 -0400

    first commit

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'phil'},
+            'body': 'sup'
+        },
+        {
+            'author': {'username': 'bill'},
+            'body': 'yo'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
